bc2806ba347901feea9ff881227c4fc507bae5a1
famartinez <famartinez@ED648.edicom.es>
2017-08-29 12:02:21 +0200

    new generic api-gatewaygit status

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,26 +1,16 @@
 const express = require('express')  
 const httpProxy = require('express-http-proxy')  
+const gatewayConfig = require('./api-gateway-config.json');
 const app = express()
 
-const userServiceProxy = httpProxy(process.env.USERS_URL || 'http://localhost:8888')
-const fruitsServiceProxy = httpProxy(process.env.FRUITS_URL || 'http://localhost:9999')
+gatewayConfig.services.forEach(function(service) {
+  console.log("mapping service "+service.name);
+  const proxy = httpProxy(service.url);
+  app.all(service.mapping,(req,res,next)=>{
+    proxy(req,res,next);
+  })
+});
 
-// Authentication
-app.use((req, res, next) => {  
-  // TODO: my authentication logic
-  next()
-})
-
-// Proxy request
-app.use()
-
-app.all('/users*', (req, res, next) => {  
-  userServiceProxy(req, res, next)
-})
-app.all('/fruits*', (req, res, next) => {  
-  fruitsServiceProxy(req, res, next)
-})
-
-app.listen(process.env.PORT || 10000, function () {
-  console.log('Gateway listening on port '+ (process.env.PORT || 10000));
+app.listen(gatewayConfig.port || process.env.PORT || 10000, function () {
+  console.log('Gateway listening on port '+ (gatewayConfig.port || process.env.PORT || 10000));
 });
